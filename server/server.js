76a64339a9049661895b44ef701f92a0128b049f
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-07 15:26:07 +0530

    fix errror

--- server/server.js
+++ server/server.js
@@ -6,40 +6,31 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import userRouter from "./routes/userRouter.js";
 
-
-
 const app = express();
 
-
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://vite-mern-auth-w4a1.vercel.app'
+];
 
 app.use(cors({
-  origin: 'https://vite-mern-auth-w4a1.vercel.app', // your Vercel frontend URL
+  origin: allowedOrigins,
   credentials: true
 }));
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
-
-
 app.use(express.json());
 app.use(cookieParser());
 
-// API endpoint
+app.get('/', (req, res) => {
+  res.send('API Working fine');
+});
 
-app.get('/',(req,res)=>{
-    res.send('API Working fine');
-})
-
-app.use('/api/auth',authRoutes);
-app.use('/api/user',userRouter)
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 4000;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
