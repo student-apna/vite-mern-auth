e28d3aae04937f26ae313c34c4c4c6eca417339b
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-07 17:14:55 +0530

    change secure:true

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,9 +28,11 @@ export const register = async (req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure :process.env.NODE_ENV==='production' ,
-            sameSite:process.env.NODE_ENV==='production'?
-            'none':'strict',
+            // secure :process.env.NODE_ENV==='production' ,
+            // sameSite:process.env.NODE_ENV==='production'?
+            // 'none':'strict',
+            secure :true,
+            sameSite :'none',
             maxAge:7 * 24 * 60 * 60 * 1000
         });
 
@@ -81,9 +83,12 @@ export const login = async (req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure :process.env.NODE_ENV==='production' ,
-            sameSite:process.env.NODE_ENV==='production'?
-            'none':'strict',
+            // secure :process.env.NODE_ENV==='production' ,
+            // sameSite:process.env.NODE_ENV==='production'?
+            // 'none':'strict',
+             secure :true,
+            sameSite :'none',
+            
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
@@ -270,8 +275,10 @@ export const resetPassword = async (req,res)=>{
  
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //   secure: process.env.NODE_ENV === 'production',
+    //   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+       secure: true,
+       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
