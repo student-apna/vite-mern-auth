19529a0d0b79ebc48b2f8577ed93e24a82757ffa
Aftab Alam <aaftabalam18191@gmail.com>
2025-07-07 17:04:18 +0530

    change lax to strict

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -83,7 +83,7 @@ export const login = async (req,res)=>{
             httpOnly:true,
             secure :process.env.NODE_ENV==='production' ,
             sameSite:process.env.NODE_ENV==='production'?
-            'none':'lax',
+            'none':'strict',
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
